67012aedc226a75642d530000e24aae8586c8b95
richielmartillo <richardbalestrim8@gmail.com>
2025-04-26 18:59:05 -0300

    Atualizar formulario estilo após melhorias visuais

--- formulario.php
+++ formulario.php
@@ -1,58 +1,53 @@
 
+<?php
+// Conexão com o banco de dados
+$conn = new mysqli("localhost", "phpuser", "1234", "projeto_php");
+
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+
+$mensagemSucesso = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nome = $_POST["nome"];
+    $mensagem = $_POST["mensagem"];
+
+    $stmt = $conn->prepare("INSERT INTO mensagens (nome, mensagem) VALUES (?, ?)");
+    $stmt->bind_param("ss", $nome, $mensagem);
+
+    if ($stmt->execute()) {
+        $mensagemSucesso = true;
+    } else {
+        echo "<p>Erro ao enviar a mensagem.</p>";
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?>
+
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Formulário com PHP</title>
+    <title>Deixe sua Mensagem</title>
     <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
-    <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nome = $_POST['nome'];
-        $mensagem = $_POST['mensagem'];
+<h2>Deixe sua mensagem:</h2>
 
-        $conn = new mysqli("localhost", "phpuser", "1234", "projeto_php");
+<form action="formulario.php" method="post">
+    <input type="text" name="nome" placeholder="Seu nome" required><br><br>
+    <textarea name="mensagem" placeholder="Sua mensagem" required></textarea><br><br>
+    <input type="submit" value="Enviar">
+</form>
 
-        if ($conn->connect_error) {
-            die("Erro de conexão: " . $conn->connect_error);
-        }
-
-        $sql = "INSERT INTO mensagens (nome, mensagem) VALUES ('$nome', '$mensagem')";
-        if ($conn->query($sql) === TRUE) {
-            echo "<p style='color:#00ff00;'>Mensagem enviada com sucesso!</p>";
-        } else {
-            echo "<p style='color:red;'>Erro ao enviar mensagem: " . $conn->error . "</p>";
-        }
-
-        $conn->close();
-    }
-    ?>
-
-    <h2>Deixe sua mensagem:</h2>
-
-    <p style="color: orange; opacity: 0.9; position: absolute; right: 20px; top: 100px; font-size: 5px; font-family: monospace;">
-  Termina ativo... Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt consectetur face
-</p>
-
-
-    <form method="post" action="formulario.php">
-        <label for="nome">Nome:</label><br>
-        <input type="text" id="nome" name="nome" required><br>
-
-        <label for="mensagem">Mensagem:</label><br>
-        <textarea id="mensagem" name="mensagem" required></textarea><br>
-
-        <input type="submit" value="Enviar">
-    </form>
-
-    <br><br>
-<a href="listar.php" class="botao-voltar">Ver Mensagens</a>
-
+<?php if ($mensagemSucesso): ?>
+    <p class="sucesso">Mensagem enviada com sucesso!</p>
+<?php endif; ?>
 
 </body>
 </html>
