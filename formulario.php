a29332cd2013e8895e15eb1e6360a73bef5a541c
Richard <richardbalestrim8@gmail.com>
2025-04-27 09:08:27 -0300

    Atualização do formulario PHP

--- formulario.php
+++ formulario.php
@@ -1,7 +1,48 @@
-
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 // Conexão com o banco de dados
-$conn = new mysqli("localhost", "phpuser", "1234", "projeto_php");
+$conn = new mysqli("localhost", "root", "817426", "formulario_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mensagens");
+
 
 if ($conn->connect_error) {
     die("Erro de conexão: " . $conn->connect_error);
@@ -18,6 +59,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     if ($stmt->execute()) {
         $mensagemSucesso = true;
+        // Redirecionar para listar.php após sucesso
+        header("Location: listar.php");
+        exit();
     } else {
         echo "<p>Erro ao enviar a mensagem.</p>";
     }
@@ -36,18 +80,15 @@ $conn->close();
     <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
+    <h2>Deixe sua mensagem:</h2>
+    <form action="formulario.php" method="post">
+        <input type="text" name="nome" placeholder="Seu nome" required><br>
+        <textarea name="mensagem" placeholder="Sua mensagem" required></textarea><br>
+        <input type="submit" value="Enviar">
+    </form>
 
-<h2>Deixe sua mensagem:</h2>
-
-<form action="formulario.php" method="post">
-    <input type="text" name="nome" placeholder="Seu nome" required><br><br>
-    <textarea name="mensagem" placeholder="Sua mensagem" required></textarea><br><br>
-    <input type="submit" value="Enviar">
-</form>
-
-<?php if ($mensagemSucesso): ?>
-    <p class="sucesso">Mensagem enviada com sucesso!</p>
-<?php endif; ?>
-
+    <?php if ($mensagemSucesso): ?>
+        <p class="sucesso">Mensagem enviada com sucesso!</p>
+    <?php endif; ?>
 </body>
 </html>
